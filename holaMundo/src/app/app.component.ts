6a270dbe9b714e525e4e632e37369d084cac598e
jamiguelg <jamiguelg@indra.es>
2017-09-12 12:18:39 +0200

    version holamundo

--- holaMundo/src/app/app.component.ts
+++ holaMundo/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Hola Mundo';
+  empresa = 'Indra';
+  numeros = '';
+  
+  constructor() {
+    for (var a = 0; a < 100; a++) {
+      this.numeros += a;
+    }
+  }  //i = 0;
+
+
+  
 }
